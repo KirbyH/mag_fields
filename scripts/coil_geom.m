15ab0a6cddb1ae78093cea270ed6b1d26fc3b5a6
matttuman <79488725+matttuman@users.noreply.github.com>
2021-02-22 23:36:34 -0600

    coi_geom.m = function to create coil geometry opt_v01.m = script to plot force objective space for varying coil/array parameters

---
+++ scripts/coil_geom.m
@@ -0,0 +1,24 @@
+function [geom] = coil_geom(hor_r, vert_r, n_p)
+% Creates geometry for a coil 
+% 
+% INPUTS : 
+%     hor_r : horizontal radius of coil [m]
+%     vert_r : vertical radius of coil [m]
+%     n_p : number of points to include in coil geometry
+% 
+% OUTPUTS : 
+%     geom : [n_p x 2] coil geometry
+%           geom(:,1) = x coordinates
+%           geom(:,2) = y coordinates
+
+theta = linspace(0,2*pi, n_p);
+
+for ii = 1:n_p
+    r = vert_r*hor_r/sqrt((hor_r*sin(theta(ii)))^2+ (vert_r*cos(theta(ii)))^2);
+    x_coord(ii,1) = r*cos(theta(ii));
+    y_coord(ii,1) = r*sin(theta(ii));
+end
+
+geom = [x_coord,y_coord];
+
+end
---
+++ scripts/opt_v01.m
@@ -0,0 +1,159 @@
+%% Plot Error Space for Optimizations
+% Matt Tuman
+% 2/22/2021
+% This script utilizes the numerous functions graciously constructed by Mr.
+% Kirby Heck to plot the objective function space for varying variables
+clear all
+close all
+
+% How fine the grid is
+density = 2^6;
+
+%% Investigate Coil Geometry 
+%{
+% Pre-allocates memmory :)
+Objective = zeros(density,density);
+% Limits for the horizontal and vertical radius
+r = linspace(0.1, 20, density);
+% Determines the number of points on the coil geometry
+n_points = 2^4+1;
+% Deterimens the Halbach radius
+halbach_r = 10;
+% Deterimes the number of coils
+n_coils = 8;
+% Determines the current
+I = 1e6;
+
+for ii = 1:density
+    for jj = 1:density
+        clear ll
+        % Calculate the coil geometry
+        geom = coil_geom(r(ii),r(jj),n_points);
+        % Calculate the Halbach Array Geometry
+        [points, coil_mp, dL] = create_halbach(geom, n_coils, halbach_r); 
+        % calculate the coil forces on each point in the Halbach Array
+        forces = coil_forces(coil_mp, dL, I); 
+        % Loop through each coil
+        for i = 1:n_coils
+            % Index to grab the correct coil points
+            span = 1+(n_points-1)*(i-1) : (n_points-1)*(i);
+            % Sum up the forces for each coil
+            sorry_kirby = sum(forces(span,:));
+            % Compute the euclidean norm of the force on each coil
+            ll(i) = norm(sorry_kirby);
+        end
+        % Sum up all of the euclidean norms on the coils for a specific
+        % coil geometry and set it to a log scale
+        Objective(jj,ii) = log(sum(ll));
+    end
+end
+
+% Plot the results
+figure
+pcolor(r,r,Objective);
+title('Force Norm Space Varying Coil Geometry')
+xlabel('Horizontal Radius [m]')
+ylabel('Vertical Radius [m]')
+colorbar
+%}
+
+%% Investigate Horizontal Coil Radius and Halbach Geometry
+%{
+clearvars -except density
+% Pre-allocates memmory :)
+Objective = zeros(density,density);
+% Limits for the horizontal and vertical radius
+r = linspace(0.1, 10, density);
+% Kept
+r_set = 1;
+% Determines the number of points on the coil geometry
+n_points = 2^4+1;
+% Deterimens the Halbach radius
+halbach_r = linspace(2,10, density);
+% Deterimes the number of coils
+n_coils = 8;
+% Determines the current
+I = 1e6;
+
+for ii = 1:density
+    for jj = 1:density
+        clear ll
+        % Calculate the coil geometry
+        geom = coil_geom(r(ii),r_set,n_points);
+        % Calculate the Halbach Array Geometry
+        [points, coil_mp, dL] = create_halbach(geom, n_coils, halbach_r(jj)); 
+        % calculate the coil forces on each point in the Halbach Array
+        forces = coil_forces(coil_mp, dL, I); 
+        % Loop through each coil
+        for i = 1:n_coils
+            % Index to grab the correct coil points
+            span = 1+(n_points-1)*(i-1) : (n_points-1)*(i);
+            % Sum up the forces for each coil
+            sorry_kirby = sum(forces(span,:));
+            % Compute the euclidean norm of the force on each coil
+            ll(i) = norm(sorry_kirby);
+        end
+        % Sum up all of the euclidean norms on the coils for a specific
+        % coil geometry and set it to a log scale
+        Objective(jj,ii) = log(sum(ll));
+    end
+end
+
+% Plot the results
+figure
+pcolor(r,halbach_r,Objective);
+title('Force Norm Space Varying Horizontal Coil Radius and Halbach Array Radius')
+xlabel('Horizontal Radius [m]')
+ylabel('Halbach Radius [m]')
+colorbar
+%}
+
+%% Investigate Vertical Coil Radius and Halbach Geometry
+
+clearvars -except density
+% Pre-allocates memmory :)
+Objective = zeros(density,density);
+% Limits for the horizontal and vertical radius
+r = linspace(0.1, 10, density);
+% Kept
+r_set = 1;
+% Determines the number of points on the coil geometry
+n_points = 2^4+1;
+% Deterimens the Halbach radius
+halbach_r = linspace(1,20, density);
+% Deterimes the number of coils
+n_coils = 8;
+% Determines the current
+I = 1e6;
+
+for ii = 1:density
+    for jj = 1:density
+        clear ll
+        % Calculate the coil geometry
+        geom = coil_geom(r_set,r(ii),n_points);
+        % Calculate the Halbach Array Geometry
+        [points, coil_mp, dL] = create_halbach(geom, n_coils, halbach_r(jj)); 
+        % calculate the coil forces on each point in the Halbach Array
+        forces = coil_forces(coil_mp, dL, I); 
+        % Loop through each coil
+        for i = 1:n_coils
+            % Index to grab the correct coil points
+            span = 1+(n_points-1)*(i-1) : (n_points-1)*(i);
+            % Sum up the forces for each coil
+            sorry_kirby = sum(forces(span,:));
+            % Compute the euclidean norm of the force on each coil
+            ll(i) = norm(sorry_kirby);
+        end
+        % Sum up all of the euclidean norms on the coils for a specific
+        % coil geometry and set it to a log scale
+        Objective(jj,ii) = log(sum(ll));
+    end
+end
+
+% Plot the results
+figure
+pcolor(r,halbach_r,Objective);
+title('Force Norm Space Varying Vertical Coil Radius and Halbach Array Radius')
+xlabel('Vertical Radius [m]')
+ylabel('Halbach Radius [m]')
+colorbar
