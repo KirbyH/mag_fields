069194de338feb52c4ccfbb3c8809ed937977d9b
matttuman <79488725+matttuman@users.noreply.github.com>
2021-02-25 23:06:33 -0600

    Does it hit??? That is the question

---
+++ scripts/does_it_hit.m
@@ -0,0 +1,13 @@
+function hit = does_it_hit(p1, p2, radius)
+
+center = 0;
+r = radius;
+
+a = norm(p2-p1);
+b = 2*((p2(1)-p1(1))*p(1) + (p2(2)-p1(2))*p(2) + (p2(3)-p1(3))*(p(3)));
+c = p1(1)^2 + p1(2)^2 + p1(3)^2
+
+
+
+
+end
