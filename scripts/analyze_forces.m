a66cd7295abf4b130989547b562a10dd41cc9f1d
Kirby <kirby.heck@gmail.com>
2021-09-19 23:06:23 -0400

    Should be complete

--- scripts/analyze_forces.m
+++ scripts/analyze_forces.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e491f3037c5013b103860fd3f2341f8e9a5fd9082a320fa964a57d25b76b230
-size 1900
+oid sha256:9e2f61409b7107537ffd4a25f8f89b8789cebc25ea674349fc2d9a82204ebef8
+size 1943
